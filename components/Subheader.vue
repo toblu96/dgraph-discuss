c5e3cb7b73eb0d0193ed095c71ffe642ebb99f7f
Tobias Blum <tobias.blum@besonet.ch>
2021-05-13 14:26:36 +0200

    Adding Vue Portals for breadcrumb navigation

--- components/Subheader.vue
+++ components/Subheader.vue
@@ -35,53 +35,8 @@
                 >
               </div>
             </li>
-            <li>
-              <div class="flex items-center">
-                <!-- Heroicon name: solid/chevron-right -->
-                <svg
-                  class="flex-shrink-0 h-5 w-5 text-gray-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <a
-                  href="#"
-                  class="ml-4 text-sm font-medium text-gray-400 hover:text-gray-200"
-                  >Engineering</a
-                >
-              </div>
-            </li>
-            <li>
-              <div class="flex items-center">
-                <!-- Heroicon name: solid/chevron-right -->
-                <svg
-                  class="flex-shrink-0 h-5 w-5 text-gray-500"
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20"
-                  fill="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    fill-rule="evenodd"
-                    d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-                <a
-                  href="#"
-                  aria-current="page"
-                  class="ml-4 text-sm font-medium text-gray-400 hover:text-gray-200"
-                  >Back End Developer</a
-                >
-              </div>
-            </li>
+            <!-- Breadcrumb extension from other pages -->
+            <portal-target name="subheader-breadcrumb"> </portal-target>
           </ol>
         </nav>
       </div>
--- pages/post/new.vue
+++ pages/post/new.vue
@@ -1,5 +1,30 @@
 <template>
   <div class="lg:px-8 p-6">
+    <!-- Breadcrumb -->
+    <portal to="subheader-breadcrumb">
+      <div class="flex items-center space-x-4">
+        <li>
+          <div class="flex items-center">
+            <!-- Heroicon name: solid/chevron-right -->
+            <svg
+              class="flex-shrink-0 h-5 w-5 text-gray-500"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+              aria-hidden="true"
+            >
+              <path
+                fill-rule="evenodd"
+                d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                clip-rule="evenodd"
+              />
+            </svg>
+            <div class="ml-4 text-sm font-medium text-gray-400">New Post</div>
+          </div>
+        </li>
+      </div>
+    </portal>
+    <!-- Content -->
     <form
       @submit.prevent="submitForm"
       class="flex flex-col justify-between space-y-8 divide-y divide-gray-200"
